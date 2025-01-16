970d18d6ff5acdda1e8a2c6b204c9f7d4de67550
Izabelly Gutierres Silva <izabellygutierressilva@email.com>
2025-01-15 23:27:26 -0300

    Criação da Repository e Controller

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,68 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank(message = "O atribuito é Obrigatório!")
+	@Size(min = 5, max = 100, message = "O atribuito texto deve conter no min 5 e max 100 caracteres. ")
+	private String titulo;
+	
+	@NotBlank(message = "O atribuito é Obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atribuito texto deve conter no min 10 e max 1000 caracteres. ")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+	
+	
+}
